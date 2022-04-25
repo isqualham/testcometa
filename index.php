bdd9ed78ef77ac1a6c7b782fba7d4ee7cd8c3941
isqualham <williamfacme@gmail.com>
2022-04-25 13:00:37 -0400

    desenrolando front end

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Teste prático cometa</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='src/styles.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <div>
+
+        <header>
+            <img src="src/image/logo.png" width="200px" height="100px" />
+            <nav>
+                <a href="#">Fornecedores</a>
+                <a href="#">Clientes</a>
+
+            </nav>            
+        </header>
+
+        <section>
+            Fornecedores
+        </section> 
+
+        <section>
+            Clientes
+        </section> 
+                
+    </div>
+</body>
+</html>
